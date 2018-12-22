06c0af6e81e396df5451ae73d0f29a0464f4c67e
arnab39 <sanu.arnab@gmail.com>
2018-12-22 16:54:51 -0500

    New folder structure

--- test/test_networks.py
+++
@@ -1,8 +0,0 @@
-import torch
-from arch import Discriminator,Discriminator2
-
-if __name__ == '__main__':
-    img = torch.randn(1,3,128,128)
-    d = Discriminator2(in_dim=3)
-    output = d(img)
-    print(output.shape)
