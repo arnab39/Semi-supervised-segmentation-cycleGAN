6ebbc0e807b9a6e75465a7e4d787c0b509da9d97
Aniket Agarwal <agarwalaniket1999@gmail.com>
2019-05-25 20:10:40 +0530

    Added the LEDNet network as a generator option

--- arch/generators.py
+++ arch/generators.py
@@ -1,7 +1,7 @@
 import functools
 import torch
 from torch import nn
-from .ops import conv_norm_relu, dconv_norm_relu, ResidualBlock, get_norm_layer, init_network
+from .ops import conv_norm_relu, dconv_norm_relu, ResidualBlock, get_norm_layer, init_network, InitialBlock, RegularBottleneck, UpsamplingBottleneck, DownsamplingBottleneck, SSnbt, Downsample_Block_led, APN
 
 
 class UnetSkipConnectionBlock(nn.Module):
@@ -247,6 +247,69 @@ class ENet(nn.Module):
         return x
 
 
+class LEDNet(nn.Module):
+    '''
+    Implementation of the LEDNet network
+    '''
+
+    def __init__(self, in_channels=3, n_classes=22, encoder_relu=False, decoder_relu=True, image_dim=128):
+        super().__init__()
+
+        ### Encoder
+        self.downsample_1 = Downsample_Block_led(in_channels, 32, kernel_size=3, padding=1, relu=encoder_relu)
+
+        self.ssnbt_1_1 = SSnbt(32, kernel_size=3, padding=1, groups=4, relu=encoder_relu)
+        self.ssnbt_1_2 = SSnbt(32, kernel_size=3, padding=1, groups=4, relu=encoder_relu)
+        self.ssnbt_1_3 = SSnbt(32, kernel_size=3, padding=1, groups=4, relu=encoder_relu)
+
+        self.downsample_2 = Downsample_Block_led(32, 64, kernel_size=4, padding=1, relu=encoder_relu)
+
+        self.ssnbt_2_1 = SSnbt(64, kernel_size=3, padding=1, groups=4, relu=encoder_relu)
+        self.ssnbt_2_2 = SSnbt(64, kernel_size=3, padding=1, groups=4, relu=encoder_relu)
+
+        self.downsample_3 = Downsample_Block_led(64, 128, kernel_size=3, padding=1, relu=encoder_relu)
+
+        self.ssnbt_3_1 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=1, relu=encoder_relu)
+        self.ssnbt_3_2 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=2, relu=encoder_relu)
+        self.ssnbt_3_3 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=5, relu=encoder_relu)
+        self.ssnbt_3_4 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=9, relu=encoder_relu)
+        self.ssnbt_3_5 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=2, relu=encoder_relu)
+        self.ssnbt_3_6 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=5, relu=encoder_relu)
+        self.ssnbt_3_7 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=9, relu=encoder_relu)
+        self.ssnbt_3_8 = SSnbt(128, kernel_size=3, padding=1, groups=8, dilated=True, dilation=17, relu=encoder_relu)
+
+        ### Decoder
+        self.apn = APN(128, n_classes, image_dim= image_dim // 8, relu=decoder_relu)
+    
+    def forward(self, x):
+
+        ### Encoder
+        x = self.downsample_1(x)
+        x = self.ssnbt_1_1(x)
+        x = self.ssnbt_1_2(x)
+        x = self.ssnbt_1_3(x)
+
+        x = self.downsample_2(x)
+        x = self.ssnbt_2_1(x)
+        x = self.ssnbt_2_2(x)
+
+        x = self.downsample_3(x)
+        x = self.ssnbt_3_1(x)
+        x = self.ssnbt_3_2(x)
+        x = self.ssnbt_3_3(x)
+        x = self.ssnbt_3_4(x)
+        x = self.ssnbt_3_5(x)
+        x = self.ssnbt_3_6(x)
+        x = self.ssnbt_3_7(x)
+        x = self.ssnbt_3_8(x)
+
+        ### Decoder
+        out = self.apn(x)
+
+        return out
+
+
+
 def define_Gen(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, gpu_ids=[0]):
     gen_net = None
     norm_layer = get_norm_layer(norm_type=norm)
@@ -262,6 +325,10 @@ def define_Gen(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, 
     elif netG == 'enet':
         ### For ENet the input_nc is always 3
         gen_net = ENet(num_classes = output_nc, encoder_relu=False, decoder_relu=True)
+    elif netG == 'lednet_128':
+        gen_net = LEDNet(in_channels=input_nc, n_classes= output_nc, encoder_relu=False, decoder_relu=True, image_dim=128)
+    elif netG == 'lednet_256':
+        gen_net = LEDNet(in_channels=input_nc, n_classes= output_nc, encoder_relu=False, decoder_relu=True, image_dim=256)
     else:
         raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
 
--- arch/ops.py
+++ arch/ops.py
@@ -342,3 +342,287 @@ class UpsamplingBottleneck(nn.Module):
         out = main+ext
 
         return self.out_activation(out)
+
+
+#####
+'''
+So here writing up the helper functions for the case of LEDNet
+'''
+
+class Channel_Split(nn.Module):
+    '''
+    Used to split the channels of a tensor
+    '''
+    def __init__(self, channels):
+        super().__init__()
+
+        self.channels = channels
+    
+    def forward(self, x):
+        x_left = x[:, 0:self.channels // 2, :, :]
+        x_right = x[:, self.channels // 2 : , :, :]
+
+        return x_left, x_right
+
+
+class ShuffleBlock(nn.Module):
+    def __init__(self, groups):
+        super(ShuffleBlock, self).__init__()
+        self.groups = groups
+
+    def forward(self, x):
+        '''Channel shuffle: [N,C,H,W] -> [N,g,C/g,H,W] -> [N,C/g,g,H,w] -> [N,C,H,W]'''
+        N,C,H,W = x.size()
+        g = self.groups
+        return x.view(N,g,C//g,H,W).permute(0,2,1,3,4).contiguous().view(N,C,H,W)
+
+
+class SSnbt(nn.Module):
+    '''
+    The class for the implementation split-shuffle-non-bottleneck block
+    '''
+
+    def __init__(self, channels, kernel_size = 3, padding = 0, groups=3, dilated=False, dilation=1, bias= False, relu=False):
+        '''
+        groups: parameter for determining the shuffling order in the shuffleBlock
+        '''
+        super().__init__()
+
+        if relu:
+            activation = nn.ReLU()
+        else:
+            activation = nn.PReLU()
+
+        self.dilated = dilated
+
+        ### Now firstly there will be channel split and hence we have to write for left and right parts of
+        ### our model
+
+        ### Also in this model the convolutions are asymmetric in nature
+
+        mid_channels = channels // 2
+
+        self.split = Channel_Split(channels)
+
+        self.l_conv1 = nn.Sequential(
+                            nn.Conv2d(mid_channels, mid_channels, kernel_size=(kernel_size, 1), stride=1, padding=(padding, 0), dilation = 1, bias=bias),
+                            activation
+        )
+
+        self.l_conv2 = nn.Sequential(
+                            nn.Conv2d(mid_channels, mid_channels, kernel_size = (1, kernel_size), stride=1, padding=(0, padding), dilation=1, bias=bias),
+                            nn.BatchNorm2d(mid_channels),
+                            activation
+        )
+
+        self.r_conv1 = nn.Sequential(
+                            nn.Conv2d(mid_channels, mid_channels, kernel_size = (1, kernel_size), stride=1, padding=(0, padding), dilation=1, bias=bias),
+                            activation
+        )
+
+        self.r_conv2 = nn.Sequential(
+                            nn.Conv2d(mid_channels, mid_channels, kernel_size = (kernel_size, 1), stride=1, padding=(padding, 0), dilation=1, bias=bias),
+                            nn.BatchNorm2d(mid_channels),
+                            activation
+        )
+
+        if dilated:
+            self.l_conv3 = nn.Sequential(
+                            nn.Conv2d(mid_channels, mid_channels, kernel_size=(kernel_size, 1), stride=1, padding=(padding + dilation - 1, 0), dilation = dilation, bias=bias),
+                            activation
+            )
+
+            self.l_conv4 = nn.Sequential(
+                                nn.Conv2d(mid_channels, mid_channels, kernel_size = (1, kernel_size), stride=1, padding=(0, padding + dilation - 1), dilation=dilation, bias=bias),
+                                nn.BatchNorm2d(mid_channels),
+                                activation
+            )
+
+            self.r_conv3 = nn.Sequential(
+                            nn.Conv2d(mid_channels, mid_channels, kernel_size = (1, kernel_size), stride=1, padding=(0, padding + dilation - 1), dilation=dilation, bias=bias),
+                            activation
+            )
+
+            self.r_conv4 = nn.Sequential(
+                                nn.Conv2d(mid_channels, mid_channels, kernel_size = (kernel_size, 1), stride=1, padding=(padding + dilation - 1, 0), dilation=dilation, bias=bias),
+                                nn.BatchNorm2d(mid_channels),
+                                activation
+            )
+        
+        self.out_activation = activation
+        self.shuffle = ShuffleBlock(groups=groups)
+    
+    def forward(self, x):
+        main = x
+    
+        l_input, r_input = self.split(x)
+
+        l_input =  self.l_conv1(l_input)
+        l_input = self.l_conv2(l_input)
+        r_input = self.r_conv1(r_input)
+        r_input = self.r_conv2(r_input)
+        if self.dilated:
+            l_input =  self.l_conv3(l_input)
+            l_input = self.l_conv4(l_input)
+            r_input = self.r_conv3(r_input)
+            r_input = self.r_conv4(r_input)
+        
+        ext = torch.cat((l_input, r_input), 1)
+
+        out = main + ext
+        out = self.out_activation(out)
+        out = self.shuffle(out)
+
+        return out
+
+
+class Downsample_Block_led(nn.Module):
+    '''
+    The downsampling block for the LEDNet
+
+    So basically we will do two operations in parallel, conv and max pool and then concat both of them
+    '''
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=0, bias=False, relu=True):
+        super().__init__()
+
+        if relu:
+            activation = nn.ReLU()
+        else:
+            activation = nn.PReLU()
+        
+        self.conv = nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels-in_channels, kernel_size=kernel_size, stride=2, padding=padding, bias=bias),
+                            activation
+        )
+
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+    
+    def forward(self, x):
+        main = self.conv(x)
+        ext = self.maxpool(x)
+
+        out = torch.cat((main, ext), 1)
+
+        return out
+
+class APN(nn.Module):
+    '''
+    This is the Attention Pyramid Network
+    '''
+
+    def __init__(self, in_channels, out_channels, image_dim, bias=False, relu=True):
+        '''
+        image_dim = dimension of the incoming image
+        '''
+        super().__init__()
+
+        if relu:
+            activation = nn.ReLU()
+        else:
+            activation = nn.PReLU()
+
+        self.conv_33 = nn.Sequential(
+                            nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=2, padding=1, bias=bias),
+                            activation
+        )
+
+        self.conv_55 = nn.Sequential(
+                            nn.Conv2d(in_channels, in_channels, kernel_size=5, stride=2, padding=2, bias=bias),
+                            activation
+        )
+
+        self.conv_77 = nn.Sequential(
+                            nn.Conv2d(in_channels, in_channels, kernel_size=7, stride=2, padding=3, bias=bias),
+                            activation
+        )
+
+        self.conv_77_toC = nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=bias),
+                            nn.BatchNorm2d(out_channels),
+                            activation
+        )
+        self.conv_77_upsample = nn.Sequential(
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias),
+                            activation
+        )
+
+        self.conv_55_toC = nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=bias),
+                            nn.BatchNorm2d(out_channels),
+                            activation
+        )
+        self.conv_55_upsample = nn.Sequential(
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias),
+                            activation
+        )
+
+        self.conv_33_toC = nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=bias),
+                            nn.BatchNorm2d(out_channels),
+                            activation
+        )
+        self.conv_33_upsample = nn.Sequential(
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias),
+                            activation
+        )
+
+
+        self.conv_11 = nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=bias),
+                            nn.BatchNorm2d(out_channels),
+                            activation
+        )
+
+        self.global_pool = nn.Sequential(
+                            nn.AvgPool2d(kernel_size = (image_dim, image_dim), stride=0, padding=0),
+                            activation
+        )
+        self.pool_conv_toC = nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=bias),
+                            nn.BatchNorm2d(out_channels),
+                            activation
+        )
+        self.pool_upsample = nn.Sequential(
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=image_dim, stride=1, padding=0, bias=bias),
+                            activation
+        )
+
+        self.final_upsample = nn.Sequential(
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias),
+                            activation,
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias),
+                            activation,
+                            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias),
+                            activation
+        )
+    
+    def forward(self, x):
+        output_conv33 = self.conv_33(x)
+        
+        output_conv55 = self.conv_55(output_conv33)
+
+        output_conv77 = self.conv_77(output_conv55)
+        output_conv77_toC = self.conv_77_toC(output_conv77)
+        output_conv77_upsample = self.conv_77_upsample(output_conv77_toC)
+
+        output_conv55_toC = self.conv_55_toC(output_conv55)
+        output_conv55_toC = output_conv55_toC + output_conv77_upsample
+        output_conv55_upsample = self.conv_55_upsample(output_conv55_toC)
+
+        output_conv33_toC = self.conv_33_toC(output_conv33)
+        output_conv33_toC = output_conv33_toC + output_conv55_upsample
+        output_conv33_upsample = self.conv_33_upsample(output_conv33_toC)
+
+        output_conv11 = self.conv_11(x)
+
+        output_global_pool = self.global_pool(x)
+        output_pool_conv_toC = self.pool_conv_toC(output_global_pool)
+        output_pool_upsample = self.pool_upsample(output_pool_conv_toC)
+
+        output_conv11 = output_conv11 * output_conv33_upsample
+        output_conv11 = output_conv11 + output_pool_upsample
+
+        final_output = self.final_upsample(output_conv11)
+
+        return final_output 
